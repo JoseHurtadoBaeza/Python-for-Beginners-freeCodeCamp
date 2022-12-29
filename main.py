6ff67a291a76df8ed5f5d9d9949b38670cd6b60d
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-12-29 04:14:49 +0100

    RPS - Refactoring and Nested If

--- main.py
+++ main.py
@@ -11,19 +11,20 @@ def check_win(player, computer):
     print(f"You chose {player}, computer chose {computer}")
     if player == computer:
         return "It's a tie!"
-    elif player == "rock" and computer == "scissors":
-        return "Rock smashes scissors! You win!"
-    elif player == "rock" and computer == "paper":
-        return "Paper covers rock! You lose."
+    elif player == "rock":
+        if computer == "scissors":
+            return "Rock smashes scissors! You win!"
+        else:
+            return "Paper covers rock! You lose."
+    elif player == "paper":
+        if computer == "rock":
+            return "Paper covers rock! You win!"
+        else: 
+            return "Scissors cuts paper! You lose."
+    elif player == "scissors":
+        if computer == "paper":
+            return "Scissors cuts paper! You win!"
+        else:
+            return "Rock smashes scissors! You lose."
 
-check_win("rock", "paper")
-
-"""age = 20
-if age >= 18:
-    print("You are an adult.")
-elif age > 12:
-    print("You are a teenager.")
-elif age > 1:
-    print("You are a child.")
-else:
-    print("You are a baby.")"""
+print(check_win("rock", "paper"))
