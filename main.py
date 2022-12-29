2659aa25f9437ce812c5280162dea10cb4f6b4e1
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-12-29 03:05:28 +0100

    RPS - Libraries, Lists, Methods

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import random
+
 def get_choices():
     player_choice = input("Enter a choice (rock, paper, scissors: ")
-    computer_choice = "paper"
+    options = ["rock", "paper", "scissor"]
+    computer_choice = random.choice(options)
     choices = {"player": player_choice, "computer": computer_choice}
     return choices
 
 choices = get_choices()
 print(choices)
+
+
+food = ["pizza", "carrots", "eggs"]
+dinner = random.choice(food)
+print(dinner)
