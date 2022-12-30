cb28455eead9f143502935d1487499a81c99395a
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-12-30 03:57:53 +0100

    Bitwise Operators

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-condition1 = True
-condition2 = False
+"""& performs binary AND 
+| performs binary OR 
+^ performs a binary XOR operation
+~ performs a binary NOT operation
+<< shift left operation
+>> shift right operation"""
 
-not condition1 #False
-condition1 and condition2 #False
-condition1 or condition2 #True
-
-print(0 or 1) ## 1
-print(False or 'hey') ## 'hey'
-print('hi' or 'hey') ## 'hi'
-print([] or False) ## 'False'
-print(False or []) ## '[]'
-
-print(0 and 1) ## 0
-print(1 and 0) ## 0
-print(False and 'hey') ## False
-print('hi' and 'hey') ## 'hey'
-print([] and False) ## []
-print(False and []) ## False
