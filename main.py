216c664820fa95ce635c1a2cba3db0177e37cfe7
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-12-30 03:34:19 +0100

    Operators

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-number = "test"
-age = int(number)
-print(isinstance(age, int))
-
-"""complex for complex numbers
-bool for booleans
-list for lists
-tuple for tuples
-range for ranges
-dict for dictionaries
-set for sets"""
+age = 20
