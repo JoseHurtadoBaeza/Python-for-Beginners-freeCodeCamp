aa5d6bc5514abef27826811bcaf44d65c63cce15
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-12-30 03:47:37 +0100

    Boolean Operators

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-a = 1
-b = 2
+condition1 = True
+condition2 = False
 
-a == b #False
-a != b #True
-a > b #False
-a <= b #True
+not condition1 #False
+condition1 and condition2 #False
+condition1 or condition2 #True
+
+print(0 or 1) ## 1
+print(False or 'hey') ## 'hey'
+print('hi' or 'hey') ## 'hi'
+print([] or False) ## 'False'
+print(False or []) ## '[]'
+
+print(0 and 1) ## 0
+print(1 and 0) ## 0
+print(False and 'hey') ## False
+print('hi' and 'hey') ## 'hey'
+print([] and False) ## []
+print(False and []) ## False
