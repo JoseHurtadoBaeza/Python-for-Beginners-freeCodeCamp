e37e2306a7e88df34d8a2f92a6461df99ca3c379
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-12-30 04:02:20 +0100

    is & in Operators

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-"""& performs binary AND 
-| performs binary OR 
-^ performs a binary XOR operation
-~ performs a binary NOT operation
-<< shift left operation
->> shift right operation"""
+is
+in
 
