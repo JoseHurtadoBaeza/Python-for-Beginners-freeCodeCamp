5766458df2cd86d15aeb9ffedf415c9cceef925c
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-12-30 03:41:34 +0100

    Comparison Operators

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-1 + -1 #0
-2 - 1 #1
-2 * 2 #4
-4 / 2 #2
-4 % 3 #1
-4 ** 2 #16
-5 // 2 #2
+a = 1
+b = 2
 
-print("Scamp" + " is a good dog")
-
-age = 8
-age += 8 # age = age + 8
-age *= 8 # age = age * 8
-print(age)
+a == b #False
+a != b #True
+a > b #False
+a <= b #True
