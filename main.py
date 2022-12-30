79309ed1f4e16e29b21fb6ccefd1c720d820e41d
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-12-30 03:39:40 +0100

    Arithmetic Operators

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-age = 20
+1 + -1 #0
+2 - 1 #1
+2 * 2 #4
+4 / 2 #2
+4 % 3 #1
+4 ** 2 #16
+5 // 2 #2
+
+print("Scamp" + " is a good dog")
+
+age = 8
+age += 8 # age = age + 8
+age *= 8 # age = age * 8
+print(age)
