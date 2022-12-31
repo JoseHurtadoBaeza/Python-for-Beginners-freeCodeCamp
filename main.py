b187bd12ef6c4518e0ade8d78170e720988d6504
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2022-12-31 02:06:48 +0100

    Control Statements

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-age = input("What is your age? ")
-print("Your age is " + age)
+condition = False
+name = "Flavio"
+
+if condition == True:
+    print("The condition")
+    print("was true")
+elif name == "Roger":
+    print("Hello Roger")
+elif name == "Syd":
+    print("Hello Syd")
+elif name == "Flavio":
+    print("Hello Flavio")
+else:
+    print("The condition")
+    print("was False")
