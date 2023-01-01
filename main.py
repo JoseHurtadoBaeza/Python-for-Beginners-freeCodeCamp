298e97bba7c549762f08949fe96eb4bb426675d7
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-01 03:58:24 +0100

    Classes

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-# Loops
+# Classes
 
-items = ["beau", "syd", "quincy"]
-for index, item in enumerate(items):
-    print(index, item)
+class Animal:
+    def walk(self):
+        print("Walking...")
+
+class Dog(Animal):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print("woof!")
+
+roger = Dog("Roger", 8)
+
+print(roger.name)
+print(roger.age)
+
+roger.bark()
+roger.walk()
