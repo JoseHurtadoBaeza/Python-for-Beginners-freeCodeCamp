6cc257b54c64ab34cc2f4a91278a4c676bda1897
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-01 23:57:12 +0100

    Añadido main para String Methods

---
+++ mainStringMethods.py
@@ -0,0 +1,20 @@
+print("person".islower())
+
+name = "Beau"
+print(name.lower())
+print(name)
+print(len(name))
+print("au" in name)
+
+
+"""
+isalpha() to check if a string contains only characters and is not empty
+isalnum() to check if a string contains characters or digits and is not empty
+isdecimal() to check if a string contains digits and is not empty
+lower() to get a lowercase version of a string
+islower() to check if a string is lowercase
+upper() to get an uppdercase version of a string
+isupper() to chekc if a string is uppercase
+title() to get a capitalized version of a string
+startswith() to check if the string starts with a specific substring
+"""
