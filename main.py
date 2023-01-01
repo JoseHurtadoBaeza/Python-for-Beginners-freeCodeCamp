54f71f879269d67a3027676c9fca342a780254bc
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-01 02:23:09 +0100

    Objects

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-# Functions
+# Objects
 
-def counter():
-    count = 0
+age = 8
 
-    def increment():
-        nonlocal count
-        count = count + 1
-        return count
-
-    return increment
-
-increment = counter()
-
-print(increment()) # 1
-print(increment()) # 2
-print(increment()) # 3
+age = age + 1
