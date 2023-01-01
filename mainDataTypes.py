f79a7c5d8d4c1630e63438bac5cb57336cc4f617
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-01 18:06:28 +0100

    Añadido main para Data Types

---
+++ mainDataTypes.py
@@ -0,0 +1,25 @@
+name = "Beau"
+print(type(name))
+print(type(name) == str)
+print(isinstance(name, str))
+
+age = float(2)
+print(isinstance(age, int))
+print(isinstance(age, float))
+
+age = int("20")
+print(isinstance(age, int))
+
+number = "20"
+age = int(number)
+print(isinstance(age, int))
+
+"""
+complex for complex numbers
+bool for booleans
+list for lists
+tuple for tuples
+range for ranges
+dict for dictionaries
+set for sets
+"""
