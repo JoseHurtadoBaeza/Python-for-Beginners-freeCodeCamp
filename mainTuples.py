005ec9aef6016febb0999f3e9dafe8598fbb02e0
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-02 04:01:29 +0100

    Añadido main para Tuples

---
+++ mainTuples.py
@@ -0,0 +1,15 @@
+# Tuples
+
+names = ("Roger", "Syd", "Beau")
+
+names[0]
+names[-1]
+names.index("Roger")
+
+len(names)
+
+print("Roger" in names)
+names[0:2]
+print(sorted(names))
+
+newTuple = names + ("Tina", "Quincy")
