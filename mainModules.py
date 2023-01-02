ec5fca6e79e4c495e0bed293094dd956742c6402
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-02 05:18:11 +0100

    Añadido main para Modules

--- mainModules.py
+++ mainModules.py
@@ -1,4 +1,40 @@
 # Modules 
+"""import dog
+
+dog.bark()
+
+from dog import bark
+
+bark()
+
+from lib import dog
+
+dog.bark()
+
+from lib.dog import bark
+
+bark()
+
+import math
+
+print(math.sqrt(4))
+
+"""
+
 from math import sqrt
 
 print(sqrt(4))
+
+"""
+math for math utilities
+re for regular expressions
+json to work with JSON
+datetime to work with dates
+sqlite3 to use SQLite
+os for Operating System utilities
+random for random number generation
+statistics for statistics utilities
+requests to perform HTTP network requets
+http to create HTTP servers
+urllib to manage URLs
+"""
