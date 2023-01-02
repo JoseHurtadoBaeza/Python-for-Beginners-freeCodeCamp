2b8e779396df2af064a8a6e1736e4e10a691fb9f
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-02 04:19:56 +0100

    Añadido main para Sets

---
+++ mainSets.py
@@ -0,0 +1,15 @@
+# Sets
+
+set1 = {"Roger", "Syd", "Roger"}
+set2 = {"Roger"}
+
+intersect = set1 & set2
+print(intersect)
+
+#mod = set1 | set2
+#mod = set1 - set2
+#mod = set1 > set2
+mod = set1 < set2
+print(mod)
+
+print(list(set1))
