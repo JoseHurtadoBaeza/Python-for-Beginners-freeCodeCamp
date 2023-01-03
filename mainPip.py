76d01cd1db69556615dfd1619011473b81f6aada
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-03 04:33:25 +0100

    Installing Packages with pip

---
+++ mainPip.py
@@ -0,0 +1,11 @@
+# pip
+
+#pypi.org
+
+#En el terminal hemos hecho -> pip install "nombre del paquete", en este caso hemos instalado el paquete requests
+
+#En el terminal podemos hacer pip install -U "nombre del paquete" para comprobar si está instalado un paquete y ver su versión
+
+#En el terminal podemos hacer pip uninstall "nombre del paquete" para desinstalar un paquete
+
+#En el terminal podemos hacer pip show "nombre del paquete" para ver información de dicho paquete
