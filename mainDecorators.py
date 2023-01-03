d543113922d4f55353c3e485e9d26c1bf003deb8
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-03 03:12:22 +0100

    Decorators

---
+++ mainDecorators.py
@@ -0,0 +1,15 @@
+# Decorators
+
+def logtime(func):
+    def wrapper():
+        print("before") # do something before
+        val = func()
+        print("after") # do something after
+        return val
+    return wrapper
+
+@logtime
+def hello():
+    print("hello")
+
+hello()
