785a2c0c4cfca9285a0c97210c9e91eb54c38a26
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-04 04:24:26 +0100

    Blackjack - Hand Class

--- Blackjack/main.py
+++ Blackjack/main.py
@@ -46,32 +46,49 @@ class Deck:
                 cards_dealt.append(card)
         return cards_dealt
 
-card1 = Card("hearts", {"rank": "J", "value": 10})
-print(card1)
-
-
-
-"""shuffle()
-
-    card = deal(1)[0]
-
-    print(card[1]["value"])
-
-    cards_dealt = deal(2)
-    card = cards_dealt[0]
-    rank = card[1]
-
-    if rank == "A":
-        value = 11
-    elif rank == "J" or rank == "Q" or rank == "K":
-        value = 10
-    else:
-        value = rank
-
-    rank_dict = {"rank": rank, "value": value}
-
-    print(rank_dict["rank"], rank_dict["value"])"""
+class Hand:
+    def __init__(self, dealer=False):
+        self.cards = []
+        self.value = 0
+        self.dealer = dealer
         
+    def add_card(self, card_list):
+        self.cards.extend(card_list)
+
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+
+        for card in self.cards:
+            card_value = int(card.rank["value"])
+            self.value += card_value
+            if card.rank["rank"] == "A":
+                has_ace = True
+
+        if has_ace and self.value > 21:
+            self.value -= 10
+
+    def get_value(self):
+        self.calculate_value()
+        return self.value
+
+    def is_blackjack(self):
+        return self.get_value() == 21
+
+    def display(self, show_all_dealer_cards=False):
+        print(f'''{"Dealer's" if self.dealer else "Your"} hand:''')
+        for index, card in enumerate(self.cards):
+            if index == 0 and self.dealer and not show_all_dealer_cards and not self.is_blackjack():
+                print("hidden")
+            else:
+                print(card)
+
+        if not self.dealer:
+            print("Value:", self.get_value())
+        print()
+
+
+
 
 
 
---
+++ Blackjack/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+hand.add_card(deck.deal(2))
